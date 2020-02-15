a4c52b1ed8d66fb2bb4d37fcfa617eec950f11b3
tseitz <tdseitz10@gmail.com>
2020-02-15 17:49:23 -0600

    Testing new stack with export

--- src/server.js
+++ src/server.js
@@ -3,12 +3,13 @@ import polka from 'polka';
 import compression from 'compression';
 import * as sapper from '@sapper/server';
 
+import './tailwind.css';
+
 const { PORT, NODE_ENV } = process.env;
 const dev = NODE_ENV === 'development';
 
 polka()
   .use(
-    '__sapper__/build',
     compression({
       threshold: 0
     }),
